2e474699e11423271376a02e2d98c4bf8fd6a67f
Fatih <156526516+FatihSik@users.noreply.github.com>
2025-01-21 22:16:58 +0000

    Create 151. Reverse Words in a String.py

---
+++ Medium/Code Code Accepted Testcase Test Result Test Result 151. Reverse Words in a String.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        output = []
+        words = s.split()
+        for i in range(len(words)):
+            output.append(words[-(i+1)])
+        return ' '.join(str(x) for x in output)
